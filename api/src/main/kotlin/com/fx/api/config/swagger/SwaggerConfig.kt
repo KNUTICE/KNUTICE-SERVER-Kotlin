276b36fb48320829f5961f4d20c0f235a4a83fb5
SEOB <svj7531@naver.com>
2025-09-02 20:17:15 +0900

    feat: Swagger security scheme 추가

--- api/src/main/kotlin/com/fx/api/config/swagger/SwaggerConfig.kt
+++ api/src/main/kotlin/com/fx/api/config/swagger/SwaggerConfig.kt
@@ -1,5 +1,10 @@
 package com.fx.api.config.swagger
 
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,4 +21,24 @@ class SwaggerConfig {
         }
     }
 
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val securityScheme = SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER)
+            .name("Authorization")
+
+        val securityRequirement = SecurityRequirement().addList("Bearer")
+
+        return OpenAPI()
+            .components(Components().addSecuritySchemes("Bearer", securityScheme))
+            .security(listOf(securityRequirement))
+            .info(
+                Info()
+                    .title("API SERVICE")
+                    .version("v1")
+            )
+    }
 }
