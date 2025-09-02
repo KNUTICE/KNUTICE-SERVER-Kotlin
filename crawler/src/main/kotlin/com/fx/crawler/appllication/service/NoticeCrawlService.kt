8fdfe2bee28b8f70f14eb3300755bf33e8bf7f62
SEOB <svj7531@naver.com>
2025-09-03 04:17:21 +0900

    feat: Crawling 성능 개선

--- crawler/src/main/kotlin/com/fx/crawler/appllication/service/NoticeCrawlService.kt
+++ crawler/src/main/kotlin/com/fx/crawler/appllication/service/NoticeCrawlService.kt
@@ -5,9 +5,11 @@ import com.fx.crawler.appllication.port.out.NoticeCrawlPort
 import com.fx.crawler.appllication.port.out.NoticePersistencePort
 import com.fx.global.domain.CrawlableType
 import com.fx.global.domain.Notice
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import kotlin.time.measureTimedValue
 
 @Service
 class NoticeCrawlService(
@@ -17,45 +19,45 @@ class NoticeCrawlService(
 
     private val log = LoggerFactory.getLogger(NoticeCrawlService::class.java)
 
-    override suspend fun crawlAndSaveNotices(crawlableTypes: List<CrawlableType>, page: Int): List<Notice> {
+    override suspend fun crawlAndSaveNotices(
+        crawlableTypes: List<CrawlableType>,
+        page: Int)
+    : List<Notice> = coroutineScope {
 
-        // 1. 목록 크롤링
-        val crawlResult = measureTimedValue {
-            crawlableTypes.flatMap {
-                log.info("Crawl target : {}", it)
-                noticeCrawlPort.crawlNoticeSummaries(it, page)
+        val allSummariesDeferred = (1..page).flatMap { p ->
+            crawlableTypes.map { type ->
+                async {
+                    log.info("Crawling page: {}, target: {}", p, type)
+                    noticeCrawlPort.crawlNoticeSummaries(type, p)
+                }
             }
         }
-        val summaries = crawlResult.value.distinctBy { it.nttId } // Set
-        log.info("Summaries crawl finished in {} ms", crawlResult.duration.inWholeMilliseconds)
-        log.info("Total : {} - Distinct total : {}", crawlResult.value.size, summaries.size)
 
-        if (summaries.isEmpty()) return emptyList()
+        val allSummaries = allSummariesDeferred.awaitAll().flatten()
+        log.info("Total summaries crawled: {}", allSummaries.size)
 
-        // 2. DB에 존재하는 nttId 조회
-        val existingIds = noticePersistencePort.findByNttIdIn(summaries.map { it.nttId } )
+        // 전체 중복 제거
+        val distinctSummaries = allSummaries.distinctBy { it.nttId }
+        if (distinctSummaries.isEmpty()) return@coroutineScope emptyList()
+
+        // DB에 이미 존재하는 nttId 확인
+        val existingIds = noticePersistencePort.findByNttIdIn(distinctSummaries.map { it.nttId })
             .map { it.nttId }.toSet()
 
-        val newNotices = summaries.filter { it.nttId !in existingIds }
-
+        val newNotices = distinctSummaries.filter { it.nttId !in existingIds }
         if (newNotices.isEmpty()) {
-            log.info("No new notices found.")
-            return emptyList()
+            log.info("No new notices found in all pages.")
+            return@coroutineScope emptyList()
         }
 
         // 3. 신규 공지의 상세 조회 - 이미지 URL 크롤링 작업
-        val detailedNoticesCrawlResult = measureTimedValue {
-            noticeCrawlPort.crawlNoticeDetails(newNotices)
-        }
-        val detailedNotices = detailedNoticesCrawlResult.value
-        log.info("Details crawl finished in {}", crawlResult.duration.inWholeMilliseconds)
+        val detailedNotices = noticeCrawlPort.crawlNoticeDetails(newNotices)
 
         // 4. DB 저장
         noticePersistencePort.saveAll(detailedNotices)
-        log.info("Saved {} new notices", detailedNotices.size)
+        log.info("Saved {} new notices from pages 1..{}", detailedNotices.size, page)
 
-        return detailedNotices
+        return@coroutineScope detailedNotices
     }
-
 
 }
