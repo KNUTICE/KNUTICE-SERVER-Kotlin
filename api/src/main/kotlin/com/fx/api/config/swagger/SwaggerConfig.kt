f4e2c65e2ad80be0d9a31bce8f1e74ccdddac8ca
SEOB <svj7531@naver.com>
2025-10-30 01:32:55 +0900

    feat: Swagger 서버 URL 환경변수 기반으로 변경 및 HTTPS 지원

--- api/src/main/kotlin/com/fx/api/config/swagger/SwaggerConfig.kt
+++ api/src/main/kotlin/com/fx/api/config/swagger/SwaggerConfig.kt
@@ -5,13 +5,18 @@ import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springdoc.core.customizers.OperationCustomizer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.HandlerMethod
 
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig(
+    @Value("\${url.scheme}") private val urlScheme: String,
+    @Value("\${url.host}") private val urlHost: String
+) {
 
     @Bean
     fun customOperationCustomizer(): OperationCustomizer {
@@ -32,9 +37,13 @@ class SwaggerConfig {
 
         val securityRequirement = SecurityRequirement().addList("Bearer")
 
+        val serverUrl = "$urlScheme://$urlHost"
+        val server = Server().url(serverUrl).description("KNUTICE-API")
+
         return OpenAPI()
             .components(Components().addSecuritySchemes("Bearer", securityScheme))
             .security(listOf(securityRequirement))
+            .servers(listOf(server))
             .info(
                 Info()
                     .title("API SERVICE")
