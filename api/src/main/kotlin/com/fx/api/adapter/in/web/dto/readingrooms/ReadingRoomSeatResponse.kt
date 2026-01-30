7ff5f819ab1daa3498404491388887737cb1e5b1
SEOB <svj7531@naver.com>
2026-01-31 00:42:36 +0900

    refactor: 개인정보 노출 관련 주석처리

--- api/src/main/kotlin/com/fx/api/adapter/in/web/dto/readingrooms/ReadingRoomSeatResponse.kt
+++ api/src/main/kotlin/com/fx/api/adapter/in/web/dto/readingrooms/ReadingRoomSeatResponse.kt
@@ -18,7 +18,7 @@ data class ReadingRoomSeatResponse(
     val column: Int, // 가로 위치
 
     val isAvailable: Boolean, // 사용 가능 여부
-    val userMaskedName: String? = null, // 사용자 마스킹 이름
+//    val userMaskedName: String? = null, // 사용자 마스킹 이름? // dslee (2026-01-31) : 이름 응답 안함
     val returnAt: LocalDateTime // 반납 시간
 
 ) {
@@ -31,7 +31,7 @@ data class ReadingRoomSeatResponse(
                 row = readingRoomSeat.row,
                 column = readingRoomSeat.column,
                 isAvailable = readingRoomSeat.isAvailable,
-                userMaskedName = readingRoomSeat.userMaskedName,
+//                userMaskedName = readingRoomSeat.userMaskedName,
                 returnAt = readingRoomSeat.returnAt
             )
 
