0c0193a985a5a6611ddf75187e52d5acc4eb1782
SEOB <svj7531@naver.com>
2026-04-13 23:44:18 +0900

    feat: College 필드 추가

--- api/src/main/kotlin/com/fx/api/adapter/in/web/dto/topic/TypeResponse.kt
+++ api/src/main/kotlin/com/fx/api/adapter/in/web/dto/topic/TypeResponse.kt
@@ -1,19 +1,22 @@
 package com.fx.api.adapter.`in`.web.dto.topic
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fx.global.domain.MajorType
 import com.fx.global.domain.MealType
 import com.fx.global.domain.NoticeType
 
+//@JsonInclude(JsonInclude.Include.NON_NULL) // dslee - null 인 필드는 응답에서 제외함
 data class TypeResponse(
     val topic: String,
-    val name: String
+    val name: String,
+    val college: String? = null
 ) {
     companion object {
         fun fromNoticeTypes(): List<TypeResponse> =
             NoticeType.entries.map { TypeResponse(topic = it.name, name = it.category) }
 
         fun fromMajorTypes(): List<TypeResponse> =
-            MajorType.entries.map { TypeResponse(topic = it.name, name = it.category) }
+            MajorType.entries.map { TypeResponse(topic = it.name, name = it.category, college = it.college) }
 
         fun fromMealTypes(): List<TypeResponse> =
             MealType.entries.map { TypeResponse(topic = it.name, name = it.category) }
