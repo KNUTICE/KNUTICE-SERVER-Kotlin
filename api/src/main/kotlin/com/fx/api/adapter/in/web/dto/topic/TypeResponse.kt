42c202784f1e2c99a30354b652f30216062e4d21
SEOB <svj7531@naver.com>
2026-04-19 23:36:52 +0900

    feat: 학과 정보 조회 다국어(KR, EN, JA) 지원

--- api/src/main/kotlin/com/fx/api/adapter/in/web/dto/topic/TypeResponse.kt
+++ api/src/main/kotlin/com/fx/api/adapter/in/web/dto/topic/TypeResponse.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fx.global.domain.MajorType
 import com.fx.global.domain.MealType
 import com.fx.global.domain.NoticeType
+import org.springframework.context.MessageSource
+import org.springframework.context.i18n.LocaleContextHolder
 
 //@JsonInclude(JsonInclude.Include.NON_NULL) // dslee - null 인 필드는 응답에서 제외함
 data class TypeResponse(
@@ -15,8 +17,16 @@ data class TypeResponse(
         fun fromNoticeTypes(): List<TypeResponse> =
             NoticeType.entries.map { TypeResponse(topic = it.name, name = it.category) }
 
-        fun fromMajorTypes(): List<TypeResponse> =
-            MajorType.entries.map { TypeResponse(topic = it.name, name = it.category, college = it.college) }
+        fun fromMajorTypes(messageSource: MessageSource): List<TypeResponse> {
+            return MajorType.entries.map {
+                val locale = LocaleContextHolder.getLocale()
+                TypeResponse(
+                    topic = it.name,
+                    name = messageSource.getMessage("topic.${it.name.lowercase()}", null, it.category, locale) ?: it.category,
+                    college = messageSource.getMessage("college.${it.college.lowercase()}", null, it.college, locale) ?: it.college
+                )
+            }
+        }
 
         fun fromMealTypes(): List<TypeResponse> =
             MealType.entries.map { TypeResponse(topic = it.name, name = it.category) }
